40ed073737c90a77782946c25b2ad87bb56cc151
Chaitanya Bhagavan <chaitanya.bhagavan@gmail.com>
2024-11-10 20:35:51 +0530

    Bump version to 1.0.2 and add DocustackButtons component type definitions

---
+++ dist/components/buttons/DocustackButtons.d.ts
@@ -0,0 +1,13 @@
+type DocustackButtonsProps = {
+    buttonColor?: string;
+    class?: string;
+    customStyle: any;
+    onSubmit: (value: string) => void;
+    data: {
+        label: string;
+        value: string;
+    }[];
+};
+export declare const DocustackButtons: (props: DocustackButtonsProps) => import("solid-js").JSX.Element;
+export {};
+//# sourceMappingURL=DocustackButtons.d.ts.map
